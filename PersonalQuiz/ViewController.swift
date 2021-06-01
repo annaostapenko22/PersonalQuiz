2d0f673350479c1a8be831ab22fe75fe44f85984
Anna Ostapenko <kvintabudinvest@gmail.com>
2021-06-01 07:55:55 +0200

    Initial Commit

---
+++ PersonalQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalQuiz
+//
+//  Created by Anna Ostapenko on 01.06.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
